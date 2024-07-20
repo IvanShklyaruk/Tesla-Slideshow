0ac9b2a20589bc1a96f1634fbd72ab500cd40bcf
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-20 01:43:31 -0400

    Code completed

---
+++ src/pages/TeslaSlideshowPage.jsx
@@ -0,0 +1,22 @@
+import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
+import { Experience } from "../components/Experience";
+import { Overlay } from "../components/Overlay";
+import "../styles/TeslaSlideshowPage.css";
+
+function TeslaSlideShowPage() {
+  return (
+    <>
+      <Leva hidden />
+      <Overlay />
+      <div className="full-screen-canvas">
+        <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
+          <color attach="background" args={["#ececec"]} />
+          <Experience />
+        </Canvas>
+      </div>
+    </>
+  );
+}
+
+export default TeslaSlideShowPage;
