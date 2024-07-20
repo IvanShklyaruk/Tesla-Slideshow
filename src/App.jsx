dec714241f6b2859d3408a87c16f7466391f11fd
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-20 01:21:04 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TeslaSlideshowPage from "./pages/TeslaSlideshowPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TeslaSlideshowPage />
+    </div>
+  );
+}
+
+export default App;
