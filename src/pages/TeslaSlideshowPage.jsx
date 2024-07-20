dcd65ba4085a06884063a97a4e9d625f48f66c76
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-20 01:55:42 -0400

    fixed errors and added loading screen

--- src/pages/TeslaSlideshowPage.jsx
+++ src/pages/TeslaSlideshowPage.jsx
@@ -2,12 +2,14 @@ import { Canvas } from "@react-three/fiber";
 import { Leva } from "leva";
 import { Experience } from "../components/Experience";
 import { Overlay } from "../components/Overlay";
+import LoadingScreen from "../components/LoadingScreen";
 import "../styles/TeslaSlideshowPage.css";
 
 function TeslaSlideShowPage() {
   return (
     <>
       <Leva hidden />
+      <LoadingScreen />
       <Overlay />
       <div className="full-screen-canvas">
         <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
